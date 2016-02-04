4ed28e1954d6586e693399b243aa36fa3c72c4ea
Zachary Satoshi Feldcamp <zachary.feldcamp@me.com>
2016-02-04 04:10:49 -0500

    begun graph algorithms

--- coin_change.cpp
+++ coin_change.cpp
@@ -1,5 +1,6 @@
 #include <limits>
 #include <climits>
+#include <vector>
 #include <iostream>
 using namespace std;
 
@@ -27,11 +28,34 @@ int numberOfSolutions(int total, int coins[], int num_coins) {
 	delete [] temp;
 	return temp[num_coins][total];
 }
-		
-int main() {
-	int total = 5;
-	int coins[3] = {1,2,3};
-	cout<<numberOfSolutions(total,coins,3)<<endl;
-	return 0;
+
+unsigned long long int make_change(int n, vector<int> M) {
+	int num_coins = M.size();
+	vector<vector<unsigned long long int>> S (num_coins+1,vector<unsigned long long int> (n+1));
+	for (int i=1; i <= num_coins; i++) {
+		for (int a=0; a <= n; a++) {
+			int m = M[i-1];
+			if (a == 0)
+				S[i][a] = 1;
+			else if (a >= m)
+				S[i][a] = S[i-1][a] + S[i][a-m];
+			else
+				S[i][a] = S[i-1][a];
+		}
+	}
+	return S[num_coins][n];
 }
 
+string vec_to_string(vector<int> vec) {
+	string s;
+	for (int &x : vec)
+		s += to_string(x) + " ";
+	return s;
+}
+		
+int main() {
+	int n = 250;
+	vector<int> M = {41,34,46,9,37,32,42,21,7,13,1,24,3,43,2,23,8,45,19,30,29,18,35,11};
+	printf("# of ways to make change for %d with %s: %llu\n", n,vec_to_string(M).data(), make_change(n,M));
+	return 0;
+}
--- graph.cpp
+++ graph.cpp
@@ -1,5 +1,69 @@
 #include <iostream>
+#include <map>
+#include <set>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+void DFS_recursive(set<char> &V, char N, map<char,set<char>> &A) {
+	for (const char &n : A[N]) {
+		if (V.find(n) == V.end()) {
+			cout<<n<<endl;
+			V.insert(n);
+			DFS_recursive(V,n,A);
+		}
+	}
+}
+
+void DFS_iterative(map<char,set<char>> &A, char v) {
+	set<char> V;
+	stack<char> S;
+	S.push(v);
+	while (!(S.empty())) {
+		v = S.top();
+		S.pop();
+		if (V.find(v) == V.end()) {
+			cout<<v<<endl;
+			V.insert(v);
+			for (const char &w : A[v]) {
+				S.push(w);
+			}
+		}
+	}
+}
 
 int main() {
+	map<char,set<char>> A;
+	A['A'].insert('B');
+	A['A'].insert('D');
+	A['A'].insert('G');
+
+	A['B'].insert('A');
+	A['B'].insert('E');
+	A['B'].insert('F');
+
+	A['C'].insert('F');
+	A['C'].insert('H');
+
+	A['D'].insert('A');
+	A['D'].insert('F');
+
+	A['E'].insert('B');
+	A['E'].insert('G');
+
+	A['F'].insert('B');
+	A['F'].insert('C');
+	A['F'].insert('D');
+
+	A['G'].insert('A');
+	A['G'].insert('E');
+
+	A['H'].insert('C');
+
+	set<char> V;
+	V.insert('A');
+	//DFS_recursive(V,'A',A);
+	DFS_iterative(A,'A');
 	return 0;
 }
